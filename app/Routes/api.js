9f7a329967ab02cec74a313917163192cac7e4d8
PENGYUXIONG <pxiong@ualberta.ca>
2019-04-01 16:34:25 +0800

    get rid some test code

--- app/Routes/api.js
+++ app/Routes/api.js
@@ -1,10 +1,5 @@
 router = require('express').Router();
 
-const initDb = require('../Db/initDb');
-
-// for each api call, make sure connect to database
-router.all('/', [initDb]);
-
 router.get('/login', (req, res)=>{
     res.send({
         status: 200,
--- app/server.js
+++ app/server.js
@@ -1,17 +1,10 @@
 const express = require('express');
 const app = express();
-
-const port = process.env.port || 8000;
+const port = 8008;
 const path = require('path');
 const router = require('./Routes/api')
 
-app.use(express.static('../../front-end/build'));
-
 app.use('/', router);
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../../front-end//src/index.html'))
-});
 
 app.listen(port, ()=>{
     console.log("server has port number of : " + port)
